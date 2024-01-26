9a470e2f0e619d763a8e5e25e1913c94f5e8b6bc
lehan2512 <hewawasam.lehan@gmail.com>
2024-01-26 14:04:35 +0530

    Add project files.

---
+++ BuildingController.cs/BuildingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G21097711
+{
+    public class BuildingController
+    {
+        public BuildingController(string id)
+        {
+
+        }
+    }
+}
